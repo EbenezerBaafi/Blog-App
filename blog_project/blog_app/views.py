275f2653883a033b56c7429d1cc7c63bafda4d78
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-09-26 23:23:01 +0000

    Adde a blog page

--- blog_project/blog_app/views.py
+++ blog_project/blog_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def home(index):
+    return HttpResponse("This is the home page of the blog application.")
+
+def blog(index):
+    return HttpResponse("This is the blog page of the blog application.")
