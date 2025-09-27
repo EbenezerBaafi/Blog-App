ace87861a8feae36e8d36a332c20c0afd01db0f8
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-09-27 23:36:06 +0000

    feat: implement blog home view and template loop to display list of posts

--- blog_project/blog_app/views.py
+++ blog_project/blog_app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from . models import Post
 
 # Create your views here.
 
 def bloghome(request):
-    return render(request, 'blog/bloghome.html')
+    allPosts = Post.objects.all()
+    context = {'allPosts': allPosts}
+    return render(request, 'blog/bloghome.html', context)
 
 def blogpost(request, slug):
     return render(request, 'blog/blogpost.html')
