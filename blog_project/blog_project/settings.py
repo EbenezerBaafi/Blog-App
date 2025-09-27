766cb55f2c33031009651a890b73e7e3cc5be5db
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-09-27 13:18:32 +0000

    feat(site): add home templates, contact model, and render-based views
    - Configure project templates directory
    
    - Add base layout and home/about/contact templates (Bootstrap)
    
    - Switch home and blog views to render templates; add home/blog URL routes
    
    - Improve contact form markup (CSRF, names, form-control, tel) and add Contact model
    
    - Generate and apply migrations for new model

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home.apps.HomeConfig',
+    'blog_app.apps.BlogAppConfig',
 ]
 
 MIDDLEWARE = [
--- blog_project/home/models.py
+++ blog_project/home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+# Table for storing the data of the contact us page
+
+class Contact(models.Model):
+    serial_number = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=15)
+    email = models.CharField(max_length=50)
+    content = models.TextField()
+    time_stamp = models.DateTimeField(auto_now_add=True, blank=True)
