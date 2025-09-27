fff5eb11fd0080d03e762b35844642977760d43c
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-09-27 22:53:58 +0000

    feat: add Post model and register it in admin panel

--- blog_project/blog_app/models.py
+++ blog_project/blog_app/models.py
@@ -2,3 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+class Post(models.Model):
+    serial_number = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=15)
+    author = models.CharField(max_length=50)
+    slug = models.TextField()
+    time_stamp = models.DateTimeField(blank=True)
+
+    def __str__(self):
+        return self.title + ' - ' + self.author
