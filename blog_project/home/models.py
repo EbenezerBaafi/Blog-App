92d1bedb8d9ef47676392f36c751a12b40f9a14a
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-09-27 13:57:02 +0000

    feat: add Contact model and enable saving contact form data from the view

--- blog_project/home/models.py
+++ blog_project/home/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Contact(models.Model):
     serial_number = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=15)
+    phone = models.CharField(max_length=15)
     email = models.CharField(max_length=50)
     content = models.TextField()
     time_stamp = models.DateTimeField(auto_now_add=True, blank=True)
--- blog_project/home/views.py
+++ blog_project/home/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render, HttpResponse
+from .models import Contact
+
 # Create your views here.
 
 def home(request):
     return render(request, 'home/home.html')
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        content = request.POST.get('content')
+        contact = Contact(name=name, email=email, phone=phone, content=content)
+        contact.save()
+        print(name, email, phone, content)
     return render(request, 'home/contact.html')
 
 def about(request):
